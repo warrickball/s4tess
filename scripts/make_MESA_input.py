35e5c5306ad3429157dbd6aa6a91c64c051e0adb
Warrick Ball <wball@bison.ph.bham.ac.uk>
2018-04-19 17:17:25 +0100

    Tried alternative formulation of Z and Z/X, plus other tweaks

--- scripts/make_MESA_input.py
+++ scripts/make_MESA_input.py
@@ -12,21 +12,32 @@ parser.add_argument('trilegal', type=str,
                     help="TRILEGAL NumPy binary with target stars")
 parser.add_argument('basename', type=str,
                     help="""base output filename for Python format statement
-                    (e.g. sector00/{:4d}/inlist_run)""")
+                    (e.g. sector00/{:04d}/inlist_run)""")
+parser.add_argument('--verbose', '-v', action='store_const', const=True,
+                    default=False, help="show progress while")
 args = parser.parse_args()
 
 # approximate formula from Rodrigues et al. (2017)
 Zsun = 0.01756
+Ysun = 0.26618
+Xsun = 1.0 - Zsun - Ysun
 Yp = 0.2485
-dY_dZ = 1.007
+dY_dZ = 1.0068337129840546697 # 1.007
 
 tri = np.load(args.trilegal)
 
 for i, row in enumerate(tri):
+    if args.verbose:
+        print('Creating star %i...' % i)
+        
     t = 10.**row['logAge']/1e9
     M = row['m_ini']
-    Z = 10.**row['M_H']*Zsun
+    Z = 10.**row['M_H']*Zsun  # as described in paper
     Y = Yp + dY_dZ*Z
+    # Z_X = 10.**row['M_H']*Zsun/Xsun
+    # X = (1.0 - Yp)/(1.0 + (1.0 + dY_dZ)*Z_X)
+    # Z = Z_X*X
+    Y = 1.0 - X - Z
     with open(args.basename.format(i), 'w') as f:
         f.writelines(['&star_job\n',
                       '/\n\n',
