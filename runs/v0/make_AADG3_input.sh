573b2cef193fc3e41594e3d98447aef0853d8569
Warrick Ball <wball@bison.ph.bham.ac.uk>
2018-08-02 15:24:55 +0100

    Update script to observe new make_AADG3 input format

--- runs/v0/make_AADG3_input.sh
+++ runs/v0/make_AADG3_input.sh
@@ -6,5 +6,5 @@ OLDPWD=$(pwd)
 # BASENAME=$(echo $1 | sed 's:/:_:g')
 BASENAME=$(basename $1)
 cd $1
-python3 ../../../../scripts/make_AADG3_input.py gyre_summary.txt $BASENAME.in $BASENAME.con $BASENAME.rot --min-H 0.0001
+python3 ../../../../scripts/make_AADG3_input.py gyre_summary.txt $BASENAME.atl $BASENAME
 cd $OLDPWD
