2c303340ef215ff2507a316bf8d19fe790e15a88
Warrick Ball <wball@bison.ph.bham.ac.uk>
2018-08-02 15:21:04 +0100

    Added radial velocity snippet

--- scripts/make_AADG3_input.py
+++ scripts/make_AADG3_input.py
@@ -5,6 +5,7 @@ from tomso import gyre
 from argparse import ArgumentParser
 # from solioak import scaling
 from collections import OrderedDict
+from hashlib import sha1  # overkill?
 import AADG3
 
 def logW_meta(nu, numax, Teff, a_alpha, b_alpha, c_alpha,
@@ -25,9 +26,8 @@ def logW_meta(nu, numax, Teff, a_alpha, b_alpha, c_alpha,
 
 parser = ArgumentParser()
 parser.add_argument('summary', type=str, help='input GYRE summary')
-parser.add_argument('namein', type=str, help='name of output Fortran namelist')
-parser.add_argument('namecon', type=str, help='name of output .con file')
-parser.add_argument('namerot', type=str, help='name of output .rot file')
+parser.add_argument('atl', type=str, help='input ATL data (for galactic longitude)')
+parser.add_argument('baseout', type=str, help='basename for output files')
 parser.add_argument('--splitting', type=float, default=-1,
                     help="constant rotation splitting, ignored if < 0, "
                     "in which case rotation is taken from summary "
@@ -35,6 +35,12 @@ parser.add_argument('--splitting', type=float, default=-1,
 parser.add_argument('--min-H', type=float, default=-1,
                     help="minimum for height H (default=-1, i.e. keep all modes)")
 args = parser.parse_args()
+
+atl = {}  # ATL data
+with open(args.atl, 'r') as f:
+    for line in f.readlines():
+        k, v = line.split('=')
+        atl[k.strip()] = float(v)
 
 header, summary = gyre.load_summary(args.summary)
 
@@ -54,6 +60,11 @@ numax = numax_sun*(M/R**2/(Teff/Teff_sun)**0.5)
 Dnu = Dnu_sun*np.sqrt(M/R**3)
 sig = np.sqrt(L**2/M**3/(Teff/Teff_sun)**5.5*(numax/numax_sun))*sig_sun
 
+namecon = args.baseout + '.con'
+namerot = args.baseout + '.rot'
+
+np.random.seed(int(sha1((args.summary + args.atl + args.baseout).encode('utf-8')).hexdigest(), 16)%2**32)
+
 namelist = OrderedDict()
 namelist['user_seed'] = np.random.randint(100, 2**28-1)
 namelist['cadence'] = 120.0
@@ -71,11 +82,11 @@ namelist['p(1)'] = 1.52355
 namelist['p(2)'] = 0.565349
 namelist['p(3)'] = 0.0361707
 namelist['ass_init'] = True
-namelist['namecon'] = args.namecon
-namelist['namerot'] = args.namerot
-namelist['nameout'] = args.namecon.replace('.con', '.asc')
+namelist['namecon'] = namecon
+namelist['namerot'] = namerot
+namelist['nameout'] = args.baseout + '.asc'
 
-AADG3.save_namelist(args.namein, namelist)
+AADG3.save_namelist(args.baseout + '.in', namelist)
 
 l = summary['l'].astype(int)
 n = summary['n_pg'].astype(int)
@@ -112,7 +123,23 @@ H = Henv*np.exp(-(nu-numax)**2/2./cenv**2)/Q
 amp2 = H*Dnu
 I = H > args.min_H
 
-np.savetxt(args.namecon, np.vstack([l, n, nu, width, amp2, 0.0*nu]).T[I],
+# Doppler shift the frequencies
+TAU = 2*np.pi
+def func(x, a0, a1, p1, a2, p2):
+    return a0 + a1*np.sin(TAU*(x/360.0 + p1)) + a2*np.sin(TAU*(x/180.0 + p2))
+
+p = [1.37622546, 18.8420428, 0.577075134, 7.93274147, -0.0148034692]
+mean_v = func(atl['GLon'], *p)
+p = [30.01595341, -1.1142313, 0.72453242, 4.34319137, 0.19605259]
+sigma_v = func(atl['GLon'], *p)
+v = mean_v + sigma_v*np.random.randn()
+c = 299792.458
+
+nu = nu*np.sqrt((1-v/c)/(1+v/c))
+
+np.savetxt(args.baseout + '.vr', [v])
+
+np.savetxt(namecon, np.vstack([l, n, nu, width, amp2, 0.0*nu]).T[I],
            fmt=['%2i','  %5i','  %12.7e','  %12.7e','  %12.7e','  %12.8e'])
 
 if args.splitting >= 0:
@@ -124,7 +151,7 @@ else:
         dnu_rot = np.zeros_like(nu)
         
 
-with open(args.namerot, 'w') as f:
+with open(namerot, 'w') as f:
     for ni, li, dnu_roti in zip(n[I], l[I], dnu_rot[I]):
         for m in range(1, li+1):
             f.write('%5i%3i%3i%12.7f\n' % (ni, li, m, dnu_roti))
