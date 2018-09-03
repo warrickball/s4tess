bf4254b46872c285fb1d5bc50ff3752a41624b49
Warrick Ball <wball@bison.ph.bham.ac.uk>
2018-09-03 09:39:18 +0100

    add_white_noise.py was spending most of it's time reading and writing to file.  Huge speedup by doing this manually.

--- scripts/add_white_noise.py
+++ scripts/add_white_noise.py
@@ -11,6 +11,8 @@ file.""")
 parser.add_argument('folder', type=str, help="folder containing files. "
                     "e.g. 'south/00000'")
 parser.add_argument('-v', '--verbose', action='store_true')
+parser.add_argument('--fmt', type=str, default='%16.7f',
+                    help="format for output lightcurve (default='%16.7f')")
 args = parser.parse_args()
 
 def vprint(msg):
@@ -19,10 +21,20 @@ def vprint(msg):
 
 basename = args.folder + '/' + args.folder.split('/')[-1]
 
-vprint('Loading metadata... ')
+vprint('Loading metadata from %s/%s.meta... ' % (args.folder, basename))
 meta = load_txt('%s.meta' % basename)
+vprint('Done.\nLoading lightcurve from %s/%s.asc... ' % (args.folder, basename))
+
+# asc = np.loadtxt('%s.asc' % basename)
+with open('%s.asc' % basename, 'r') as f:
+    asc = np.array([float(line) for line in f.readlines()])
+
 vprint('Done.\nAdding white noise to lightcurve... ')
-asc = np.loadtxt('%s.asc' % basename)
 asc = asc + np.random.randn(len(asc))*meta['sigma']
-np.savetxt('%s_WN.asc' % basename, asc, fmt='%16.7f')
+vprint('Done.\nSaving lightcurve to %s/%s... ' % (args.folder, basename))
+
+# np.savetxt('%s_WN.asc' % basename, asc, fmt='%16.7f')
+with open('%s_WN.asc' % basename, 'w') as f:
+    f.writelines([(args.fmt + '\n') % asci for asci in asc])
+
 vprint('Done.\n')
